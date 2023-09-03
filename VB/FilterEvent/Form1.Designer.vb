b00a7b219ef5a31a3910e41b82459a1acb52bbf2
DevExpressExampleBot <fakeEmail>
2023-09-03 15:59:18 +0400

    Source auto update [skip ci]

--- VB/FilterEvent/Form1.Designer.vb
+++ VB/FilterEvent/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace FilterEvent
-    Partial Public Class Form1
+Namespace FilterEvent
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,14 +12,14 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+'#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
@@ -36,10 +38,10 @@
             Me.barDockControlBottom = New DevExpress.XtraBars.BarDockControl()
             Me.barDockControlLeft = New DevExpress.XtraBars.BarDockControl()
             Me.barDockControlRight = New DevExpress.XtraBars.BarDockControl()
-            DirectCast(Me.GridControl, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.GridView, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.barManager1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.repositoryItemComboBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.GridControl), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.GridView), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.barManager1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.repositoryItemComboBox1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' GridControl
@@ -53,13 +55,14 @@
             Me.GridControl.ShowOnlyPredefinedDetails = True
             Me.GridControl.Size = New System.Drawing.Size(552, 401)
             Me.GridControl.TabIndex = 0
-            Me.GridControl.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.GridView})
+            Me.GridControl.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView})
             ' 
             ' GridView
             ' 
-            Me.GridView.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colShipName, Me.colOrderDate, Me.colFreight})
+            Me.GridView.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colShipName, Me.colOrderDate, Me.colFreight})
             Me.GridView.GridControl = Me.GridControl
             Me.GridView.Name = "GridView"
+            AddHandler Me.GridView.SubstituteFilter, New System.EventHandler(Of DevExpress.Data.SubstituteFilterEventArgs)(AddressOf Me.GridView_SubstituteFilter)
             ' 
             ' colShipName
             ' 
@@ -88,15 +91,15 @@
             ' 
             ' barManager1
             ' 
-            Me.barManager1.Bars.AddRange(New DevExpress.XtraBars.Bar() { Me.bar1})
+            Me.barManager1.Bars.AddRange(New DevExpress.XtraBars.Bar() {Me.bar1})
             Me.barManager1.DockControls.Add(Me.barDockControlTop)
             Me.barManager1.DockControls.Add(Me.barDockControlBottom)
             Me.barManager1.DockControls.Add(Me.barDockControlLeft)
             Me.barManager1.DockControls.Add(Me.barDockControlRight)
             Me.barManager1.Form = Me
-            Me.barManager1.Items.AddRange(New DevExpress.XtraBars.BarItem() { Me.beiShowByYear})
+            Me.barManager1.Items.AddRange(New DevExpress.XtraBars.BarItem() {Me.beiShowByYear})
             Me.barManager1.MaxItemId = 1
-            Me.barManager1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() { Me.repositoryItemComboBox1})
+            Me.barManager1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.repositoryItemComboBox1})
             ' 
             ' bar1
             ' 
@@ -104,7 +107,7 @@
             Me.bar1.DockCol = 0
             Me.bar1.DockRow = 0
             Me.bar1.DockStyle = DevExpress.XtraBars.BarDockStyle.Top
-            Me.bar1.LinksPersistInfo.AddRange(New DevExpress.XtraBars.LinkPersistInfo() { New DevExpress.XtraBars.LinkPersistInfo(Me.beiShowByYear)})
+            Me.bar1.LinksPersistInfo.AddRange(New DevExpress.XtraBars.LinkPersistInfo() {New DevExpress.XtraBars.LinkPersistInfo(Me.beiShowByYear)})
             Me.bar1.Text = "Tools"
             ' 
             ' beiShowByYear
@@ -116,12 +119,13 @@
             Me.beiShowByYear.Name = "beiShowByYear"
             Me.beiShowByYear.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.Caption
             Me.beiShowByYear.Width = 100
+            AddHandler Me.beiShowByYear.EditValueChanged, New System.EventHandler(AddressOf Me.beiShowByYear_EditValueChanged)
             ' 
             ' repositoryItemComboBox1
             ' 
             Me.repositoryItemComboBox1.AutoHeight = False
-            Me.repositoryItemComboBox1.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
-            Me.repositoryItemComboBox1.Items.AddRange(New Object() { "1994", "1995", "1996"})
+            Me.repositoryItemComboBox1.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+            Me.repositoryItemComboBox1.Items.AddRange(New Object() {"1994", "1995", "1996"})
             Me.repositoryItemComboBox1.Name = "repositoryItemComboBox1"
             ' 
             ' barDockControlTop
@@ -165,30 +169,39 @@
             Me.Margin = New System.Windows.Forms.Padding(4)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(Me.GridControl, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.GridView, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.barManager1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.repositoryItemComboBox1, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.GridControl), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.GridView), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.barManager1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.repositoryItemComboBox1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+'#End Region
         Private GridControl As DevExpress.XtraGrid.GridControl
-        Private WithEvents GridView As DevExpress.XtraGrid.Views.Grid.GridView
-        Private colShipName As DevExpress.XtraGrid.Columns.GridColumn
-        Private colOrderDate As DevExpress.XtraGrid.Columns.GridColumn
-        Private colFreight As DevExpress.XtraGrid.Columns.GridColumn
-        Private barManager1 As DevExpress.XtraBars.BarManager
-        Private bar1 As DevExpress.XtraBars.Bar
-        Private WithEvents beiShowByYear As DevExpress.XtraBars.BarEditItem
-        Private repositoryItemComboBox1 As DevExpress.XtraEditors.Repository.RepositoryItemComboBox
-        Private barDockControlTop As DevExpress.XtraBars.BarDockControl
-        Private barDockControlBottom As DevExpress.XtraBars.BarDockControl
-        Private barDockControlLeft As DevExpress.XtraBars.BarDockControl
-        Private barDockControlRight As DevExpress.XtraBars.BarDockControl
 
+        Private GridView As DevExpress.XtraGrid.Views.Grid.GridView
+
+        Private colShipName As DevExpress.XtraGrid.Columns.GridColumn
+
+        Private colOrderDate As DevExpress.XtraGrid.Columns.GridColumn
+
+        Private colFreight As DevExpress.XtraGrid.Columns.GridColumn
+
+        Private barManager1 As DevExpress.XtraBars.BarManager
+
+        Private bar1 As DevExpress.XtraBars.Bar
+
+        Private beiShowByYear As DevExpress.XtraBars.BarEditItem
+
+        Private repositoryItemComboBox1 As DevExpress.XtraEditors.Repository.RepositoryItemComboBox
+
+        Private barDockControlTop As DevExpress.XtraBars.BarDockControl
+
+        Private barDockControlBottom As DevExpress.XtraBars.BarDockControl
+
+        Private barDockControlLeft As DevExpress.XtraBars.BarDockControl
+
+        Private barDockControlRight As DevExpress.XtraBars.BarDockControl
     End Class
 End Namespace
-
--- VB/FilterEvent/Form1.vb
+++ VB/FilterEvent/Form1.vb
@@ -1,42 +1,40 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.Data.Filtering
 Imports DevExpress.Data
 
 Namespace FilterEvent
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-            Dim ds As New DataSet()
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            Dim ds As DataSet = New DataSet()
             ds.ReadXml("nwind.xml")
-            Me.GridControl.DataSource = ds
-            Me.GridControl.DataMember = "Orders"
-            Me.GridView.ActiveFilterCriteria = CriteriaOperator.Parse("Freight > 0")
+            GridControl.DataSource = ds
+            GridControl.DataMember = "Orders"
+            GridView.ActiveFilterCriteria = CriteriaOperator.Parse("Freight > 0")
         End Sub
 
-        Private Sub GridView_SubstituteFilter(ByVal sender As Object, ByVal e As SubstituteFilterEventArgs) Handles GridView.SubstituteFilter
-            e.Filter = e.Filter And CriteriaOperator.Parse("getyear(OrderDate) = ?", Convert.ToInt32(Me.beiShowByYear.EditValue))
+        Private Sub GridView_SubstituteFilter(ByVal sender As Object, ByVal e As SubstituteFilterEventArgs)
+            e.Filter = e.Filter And CriteriaOperator.Parse("getyear(OrderDate) = ?", Convert.ToInt32(beiShowByYear.EditValue))
         End Sub
 
-        Private Sub beiShowByYear_EditValueChanged(ByVal sender As Object, ByVal e As EventArgs) Handles beiShowByYear.EditValueChanged
-            Dim filter As CriteriaOperator = Me.GridView.ActiveFilterCriteria
-            Me.GridView.BeginDataUpdate()
+        Private Sub beiShowByYear_EditValueChanged(ByVal sender As Object, ByVal e As EventArgs)
+            Dim filter As CriteriaOperator = GridView.ActiveFilterCriteria
+            GridView.BeginDataUpdate()
             Try
-                Me.GridView.ActiveFilterCriteria = Nothing
-                Me.GridView.ActiveFilterCriteria = filter
+                GridView.ActiveFilterCriteria = Nothing
+                GridView.ActiveFilterCriteria = filter
             Finally
-                Me.GridView.EndDataUpdate()
+                GridView.EndDataUpdate()
             End Try
         End Sub
     End Class
